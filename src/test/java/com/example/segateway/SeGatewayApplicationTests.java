8366e517831742a9c88129f90bc919f00dcb00ca
JasmineYil <71649452+JasmineYil@users.noreply.github.com>
2024-04-28 21:41:55 +0200

    Added GatewayApplication, Routes and needed Dependencies

---
+++ src/test/java/com/example/segateway/SeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.segateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
